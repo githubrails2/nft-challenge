a0739dee09e4988b551738f7e45b46491eeee52f
Bill White <soniclinuxknight@hotmail.com>
2022-05-03 04:51:57 -0500

    First Message

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>NFT Drop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="">Welcome to NFT Drop Challenge</h1>
+    </div>
+  )
+}
+
+export default Home
